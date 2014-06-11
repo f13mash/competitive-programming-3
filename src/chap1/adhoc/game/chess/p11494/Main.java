42ffc88c3b5ce8129beaf210cfc1d38f9ece591f
Mahesh Gondi <mashf13@gmail.com>
2014-06-11 18:07:31 +0530

    uva p11494

--- src/chap1/adhoc/game/chess/p11494/Main.java
+++ src/chap1/adhoc/game/chess/p11494/Main.java
@@ -1,4 +1,4 @@
-package chap1.adhoc.game.card;
+package chap1.adhoc.game.chess.p11494;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,8 +19,17 @@ public class Main {
 	static String out = "";
 
 	public static void main(String[] args) {
-		System.out.println("KABIR");
-		
+		br=new BufferedReader(new InputStreamReader(System.in));
+		String line = null;
+		line = readLine();
+
+		while(line != null && line.length()!=0) {
+            if(process(line)==0) {
+            	return;
+            }
+            
+			line = readLine();
+		}
 	}
 	
 	static String readLine(){
@@ -39,10 +49,26 @@ public class Main {
 		}
 		return ret;
 	}
-	
 	static int process(String line) {
-		System.out.println("KABIR");
-		return 0;
+		int[] arr = giveArray(line.trim().split(" "));
+		if(arr[0] == 0 && arr[1] == 0 && arr[2] == 0 && arr[3] == 0)
+			return 0;
+		
+		int dx = Math.abs(arr[0] - arr[2]);
+		int dy = Math.abs(arr[1] - arr[3]);
+		
+		int cnt = 0;
+		if(dx > 0  || dy > 0) {
+			if(dx > 0 && dy > 0) {
+				dx -= dy; dy = 0;
+				cnt++;				
+			}
+			if(dx != 0  || dy != 0) {
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+		return 1;
 	}
 
 }
