08c626c02648ce71ff7da87e8ed82ba90f59ce69
f13mash <mashf13@gmail.com>
2014-07-15 15:04:00 +0530

    +p10812 solved

---
+++ src/chap1/adhoc/reallife/easy/p10812/Main.java
@@ -0,0 +1,90 @@
+package chap1.adhoc.reallife.easy.p10812;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Set;
+import java.util.SortedSet;
+
+public class Main {
+	static BufferedReader br=null;
+	/**
+	 * @param args
+	 */
+	static String out = "";
+
+	public static void main(String[] args) {
+		br=new BufferedReader(new InputStreamReader(System.in));
+		
+		String line = null;
+		line = readLine();
+
+		while(line != null && line.length()!=0) {
+            if(process(line)==0) {
+            	return;
+            }
+            
+			line = readLine();
+		}
+	}
+	
+	static String readLine(){
+		try {
+			return br.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	static int[] giveArray(String[] str) {
+		int[] ret = new int[str.length];
+		for(int i = 0; i < str.length; i++) {
+			if(str[i].compareTo("") != 0)
+				ret[i] = Integer.parseInt(str[i].trim());
+		}
+		return ret;
+	}
+	static List<Integer> giveArrayList(String[] str) {
+		List<Integer> ret = new ArrayList<>();
+		for(int i = 0; i < str.length; i++) {
+			if(str[i].compareTo("") != 0)
+				ret.add(Integer.parseInt(str[i].trim()));
+		}
+		return ret;
+	}
+	
+	static int process(String line) {
+		int cases = Integer.parseInt(line.trim());
+		while(cases-- > 0) {
+			int[] arr = giveArray(readLine().trim().split(" "));
+			int a = arr[0] + arr[1];
+			int b = arr[0] - arr[1];
+			if(a % 2 == 0 && b % 2 == 0 && a >= 0 && b >= 0) {
+				if(a < b) {
+					int t = b;
+					b = a;
+					a = t;
+				}
+				System.out.println((a/2) + " " + (b/2));
+			}
+			else {
+				System.out.println("impossible");
+			}
+		}
+		return 1;
+	}
+
+}
